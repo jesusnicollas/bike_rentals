d09d27ccb1046282e4a5658c3398303cae64ba02
Nicollas de Jesus <111331193+jesusnicollas@users.noreply.github.com>
2024-11-12 04:45:24 -0300

    Aplicação Express para salvar logs da Bike Rentals - Aluguel de bicicletas.

---
+++ src/services/logService.js
@@ -0,0 +1,24 @@
+// src/services/logService.js
+const LogModel = require('../models/logModel');
+
+const LogService = {
+    getLogs: async () => {
+        return new Promise((resolve, reject) => {
+            LogModel.getAllLogs((err, results) => {
+                if (err) reject(err);
+                else resolve(results);
+            });
+        });
+    },
+
+    addLog: async (log) => {
+        return new Promise((resolve, reject) => {
+            LogModel.createLog(log, (err, results) => {
+                if (err) reject(err);
+                else resolve(results);
+            });
+        });
+    }
+};
+
+module.exports = LogService;
